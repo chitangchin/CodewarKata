b479e3e0fe9d32874456319ea80a29f2a417fad6
Brandon-PC <Brandon-PC@DESKTOP-BS6RDCD>
2024-08-04 19:29:13 -0400

    Add project files.

---
+++ Kata/EvenOrOdd.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Kata.EvenOrOdd
+{
+    public class SolutionClass
+    {
+        public static string EvenOrOdd(int number) => (number%2==0)?"Even":"Odd";
+    }
+}
---
+++ KataTest/EvenOrOddTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Kata.EvenOrOdd;
+using NUnit.Framework;
+
+namespace Solution
+{
+    [TestFixture]
+    public class EvenOrOddTests
+    {
+        [TestCase(1)]
+        [TestCase(7)]
+        public void PositiveOddTest(int num)
+        {
+            Assert.AreEqual("Odd", SolutionClass.EvenOrOdd(num));
+        }
+
+        [TestCase(2)]
+        [TestCase(42)]
+        public void PositiveEvenTest(int num)
+        {
+            Assert.AreEqual("Even", SolutionClass.EvenOrOdd(num));
+        }
+
+        [TestCase(-1)]
+        [TestCase(-7)]
+        public void NegativeOddTest(int num)
+        {
+            Assert.AreEqual("Odd", SolutionClass.EvenOrOdd(num));
+        }
+
+        [TestCase(-2)]
+        [TestCase(-42)]
+        public void NegativeEvenTest(int num)
+        {
+            Assert.AreEqual("Even", SolutionClass.EvenOrOdd(num));
+        }
+
+        [Test]
+        public void ZeroIsEvenTest()
+        {
+            Assert.AreEqual("Even", SolutionClass.EvenOrOdd(0));
+        }
+    }
+}
