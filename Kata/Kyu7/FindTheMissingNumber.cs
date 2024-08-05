4bc471ba6eded07551cf807a25979b16c78261cb
chitangchin <chitangb.chin@gmail.com>
2024-08-05 16:56:16 -0400

    Completed Find The Missing Number

--- Kata/Kyu7/FindTheMissingNumber.cs
+++ Kata/Kyu7/FindTheMissingNumber.cs
@@ -10,7 +10,18 @@ namespace Kata.Kyu7.FindTheMissingNumber
     {
         public static int MissingNo(int[] nums)
         {
-            return 0;
+            //Given unsorted array containging all integers from 0 - 100
+            //One number is missing
+            //Write a funciton to find and return this number
+            int result = 5050;
+
+            //Bruteforce approach: iterate through all 100 numbers O(100) at most time complexity is constant and not scaling
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result -= nums[i];
+                //find the summation of 1 to 100: 5050 = (n * (n + 1)) / 2 where n = 100
+            }
+            return result;
         }
     }
 }
