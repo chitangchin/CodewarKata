759c6997e0f6001c8b3167e2bfac0e58a29528b4
chitangchin <chitangb.chin@gmail.com>
2024-08-05 16:27:34 -0400

    Setting Up for Kyu 7 Collection

---
+++ Kata/Kyu7/FindTheMissingNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Kyu7.FindTheMissingNumber
+{
+    public class SolutionClass
+    {
+        public static int MissingNo(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
