b0117fdbb16a9314ae66dec4b769ad7fdf1db60a
chitangchin <chitangb.chin@gmail.com>
2024-08-05 09:26:08 -0400

    Completed Kata Kyu 8 Multiply and Quadrant

---
+++ Kata/Quadrant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Quadrant
+{
+public class SolutionClass
+    {
+        public static int Quadrant(int x, int y)
+        {
+            /*
+             Find the quadrant the x and y exist in
+            first quadrant = +x +y
+            second quadrant = -x +y
+            third quadrant = -x -y
+            fourth quadrant = +x -y
+
+            if (x && y > 0)
+            else if (x && y < 0)
+            else if (x > 0)
+            else ()
+             
+             */
+            int quadrantNumber = 0;
+
+            if (x > 0 &&  y > 0)
+            {
+                quadrantNumber = 1;
+            }
+            if (x < 0 && y > 0)
+            {
+                quadrantNumber = 2;
+            }
+            if (x < 0 && y < 0)
+            {
+                quadrantNumber = 3;
+            }
+            if (x > 0 && y < 0)
+            {
+                quadrantNumber = 4;
+            }
+            return quadrantNumber ;
+        }
+    }
+}
---
+++ KataTest/QuadrantTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Solution
+{
+    using NUnit.Framework;
+    using System;
+    using Kata.Quadrant;
+
+    [TestFixture]
+    public class QuadrantTests
+    {
+        static void Act(int x, int y, int expected) => Assert.AreEqual(expected, SolutionClass.Quadrant(x, y), $"Quadrant( x = {x}, y = {y} )");
+
+        [TestCase(1, 2, 1)]
+        [TestCase(3, 5, 1)]
+        [TestCase(-10, 100, 2)]
+        [TestCase(-1, -9, 3)]
+        [TestCase(19, -56, 4)]
+        [TestCase(1, 1, 1)]
+        [TestCase(-60, -12, 3)]
+        public void FixedTests(int x, int y, int expected) => Act(x, y, expected);
+    }
+}
