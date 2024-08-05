ed9ebf65c9827443ca3c23e8ca8c515517c6791a
chitangchin <chitangb.chin@gmail.com>
2024-08-05 09:26:08 -0400

    Completed Kata Kyu 8 Multiply and Quadrant

---
+++ Kata/Multiply.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Multiply
+{
+    public class CustomMath
+    {
+        public static int multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
---
+++ KataTest/MultiplyTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using Kata.Multiply;
+namespace Solution
+{
+[TestFixture]
+public class CustomMathTests
+{
+    [Test]
+    public void ShouldMultiple()
+    {
+        Assert.AreEqual(2, CustomMath.multiply(2, 1));
+        Assert.AreEqual(8, CustomMath.multiply(2, 4));
+    }
+}
+
+}
