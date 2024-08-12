24be8f86f154f3fe1647233dffe54a7ef3e18327
chitangchin <chitangb.chin@gmail.com>
2024-08-12 14:41:46 -0400

    Completed a Kyu 5 question convert decimal to hexadecimal

---
+++ Kata/Kyu5/RGBToHexConversion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Kyu5
+{
+    public class SolutionClass
+    {
+        public static string Rgb(int r, int g, int b)
+        {
+            //Understand how does RGB get converted to hexadecimal?
+            /*
+             * Brute Force Approach
+            string firstDec = r.ToString("X");
+            string secondDec = g.ToString("X");
+            string thirdDec = b.ToString("X");
+            if (r >= 255) firstDec = "FF";
+            if (g >= 255) secondDec = "FF";
+            if (b >= 255) thirdDec = "FF";
+            if (r <= 0) firstDec = "00";
+            if (g <= 0) secondDec = "00";
+            if (b <= 0) thirdDec = "00";
+            if (r > 0 && r < 16) firstDec = "0" + firstDec;
+            if (g > 0 && g < 16) secondDec = "0" + secondDec;
+            if (b > 0 && b < 16) thirdDec = "0" + thirdDec;
+            return firstDec + secondDec + thirdDec;
+            */
+
+            //Optimized code
+            if (r < 0) r = 0;
+            if (r > 255) r = 255;
+            if (g < 0) g = 0;
+            if (g > 255) g = 255;
+            if (b < 0) b = 0;
+            if (b > 255) b = 255;
+            
+            //Format the string in hexadecimal using X2 arg with ToString() method
+            return r.ToString("X2") + g.ToString("X2") + b.ToString("X2");
+        }
+    }
+}
---
+++ KataTest/Kyu5Tests/RGBToHexConversionTest.cs
@@ -0,0 +1,21 @@
+﻿
+namespace KataTests.Kyu5
+{
+    using NUnit.Framework;
+    using Kata.Kyu5;
+    [TestFixture]
+    public class RGBToHexConverstionTests
+    {
+        [Test]
+        public void FixedTests()
+        {
+            Assert.That(SolutionClass.Rgb(148, -20, 211), Is.EqualTo("9400D3"), "Handle negative numbers.");
+            Assert.That(SolutionClass.Rgb(255, 255, 255), Is.EqualTo("FFFFFF"));
+            Assert.That(SolutionClass.Rgb(255, 255, 300), Is.EqualTo("FFFFFF"));
+            Assert.That(SolutionClass.Rgb(0, 0, 0), Is.EqualTo("000000"));
+            Assert.That(SolutionClass.Rgb(148, 0, 211), Is.EqualTo("9400D3"));
+            Assert.That(SolutionClass.Rgb(144, 195, 212), Is.EqualTo("90C3D4"));
+            Assert.That(SolutionClass.Rgb(212, 53, 12), Is.EqualTo("D4350C"), "Consider single hex digit numbers.");
+        }
+    }
+}
