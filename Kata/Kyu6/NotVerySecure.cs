76515ea8116fc24768784b9c2a2069fcf8d22ce3
chitangchin <chitangb.chin@gmail.com>
2024-08-12 15:24:42 -0400

    NotVerySecure Completed

---
+++ Kata/Kyu6/NotVerySecure.cs
@@ -0,0 +1,30 @@
+﻿/*
+In this example you have to validate if a user input string is alphanumeric. 
+The given string is not nil/null/NULL/None, so you don't have to check that.
+
+The string has the following conditions to be alphanumeric:
+
+At least one character ("" is not valid)
+Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+No whitespaces / underscore
+
+ */
+
+using System.Text.RegularExpressions;
+
+namespace Kata.Kyu6
+{
+    public class SolutionClass
+    {
+        public static bool Alphanumeric(string str) => new Regex("^[a-zA-Z0-9]+$").Match(str).Success;
+    
+            /*
+             * Brute Force
+            if (str.Length == 0) return false;
+            string pattern = "[^0-9a-zA-Z]+";
+            string replacement = "";
+            string result = Regex.Replace(str, pattern, replacement);
+            return result == str;
+            */
+    }
+}
---
+++ KataTest/Kyu6Tests/NotVerySecureTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Solution 
+{
+    using Kata.AbbrevName;
+    using NUnit.Framework;
+    using Kata.Kyu6;
+    using System.Collections.Generic;
+
+  [TestFixture]
+  public class Sample_Tests
+  {
+    private static IEnumerable<TestCaseData> testCases
+    {
+      get
+      {
+        yield return new TestCaseData("Mazinkaiser").Returns(true);
+        yield return new TestCaseData("hello world_").Returns(false);
+        yield return new TestCaseData("PassW0rd").Returns(true);
+        yield return new TestCaseData("     ").Returns(false);
+      }
+    }
+
+        [Test, TestCaseSource("testCases")]
+    public bool Test(string str) => Kata.Kyu6.SolutionClass.Alphanumeric(str);
+  }
+}
