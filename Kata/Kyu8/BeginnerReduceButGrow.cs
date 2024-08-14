7ac3c6794703cd7864214a67bbd6ba881d0e77af
chitangchin <chitangb.chin@gmail.com>
2024-08-14 14:59:51 -0400

    Beginner Reduce But Grow Kyu 8

---
+++ Kata/Kyu8/BeginnerReduceButGrow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.BeginnerReduceButGrow
+{
+    using System.Linq;
+    public class SolutionClass
+    {
+        public static int Grow(int[] x)
+        {
+            return x.Aggregate(1, (acc, x) => acc * x); ;
+        }
+    }
+}
---
+++ KataTest/Kyu8Tests/BeginnerReduceButGrowTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTest.Kyu8
+{
+    using NUnit.Framework;
+    using Kata.BeginnerReduceButGrow;
+
+    [TestFixture]
+    public class BeginnerReduceButGrowTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.That(SolutionClass.Grow(new[] { 1, 2, 3 }), Is.EqualTo(6));
+            Assert.That(SolutionClass.Grow(new[] { 4, 1, 1, 1, 4 }), Is.EqualTo(16));
+            Assert.That(SolutionClass.Grow(new[] { 2, 2, 2, 2, 2, 2 }), Is.EqualTo(64));
+        }
+    }
+}
