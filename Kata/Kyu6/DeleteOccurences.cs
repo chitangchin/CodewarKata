4292badd75674996f637e4aa2142735fb5649f44
chitangchin <chitangb.chin@gmail.com>
2024-08-27 15:08:57 -0400

    Kyu 6: Delete Occurrences

---
+++ Kata/Kyu6/DeleteOccurences.cs
@@ -0,0 +1,45 @@
+﻿namespace Kata.Kyu6.DeleteOccurences
+{
+    public class SolutionClass
+    {
+        public static int[] DeleteNth(int[] arr, int x)
+        {
+            //Create a dictionary to keep track of count;
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            //Track how big the result array should be
+            int xOccurenceElements = arr.Length;
+
+            //Iterate through the array
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //add the key only if it is greater than x;
+                if (map.ContainsKey(arr[i]) && map[arr[i]] == x)
+                {
+                    arr[i] = -1;
+                    xOccurenceElements--;
+                }
+                else
+                {
+                    if (map.ContainsKey(arr[i])) map[arr[i]]++;
+                    else map[arr[i]] = 1;
+                }
+            }
+
+            //Create a result int array
+            int[] resultArr = new int[xOccurenceElements];
+
+            int resultIndex = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    resultArr[resultIndex] = arr[i];
+                    resultIndex++;
+                }
+            }
+            
+            return resultArr;
+        }
+    }
+}
---
+++ KataTest/Kyu6/DeleteOccurencesTest.cs
@@ -0,0 +1,27 @@
+﻿using Kata.Kyu6.DeleteOccurences;
+
+namespace KataTest.Kyu6
+{
+    public class DeleteOccurencesTest
+    {
+        [Test]
+        public void TestSimple()
+        {
+            var expected = new int[] { 20, 37, 21 };
+
+            var actual = SolutionClass.DeleteNth(new int[] { 20, 37, 20, 21 }, 1);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestSimple2()
+        {
+            var expected = new int[] { 1, 1, 3, 3, 7, 2, 2, 2 };
+
+            var actual = SolutionClass.DeleteNth(new int[] { 1, 1, 3, 3, 7, 2, 2, 2, 2 }, 3);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
