0b51240f2c3df1dae09670d78a26d835237acf8a
chitangchin <chitangb.chin@gmail.com>
2024-08-29 09:55:07 -0400

    Kyu 7: Printer Errors

---
+++ Kata/Kyu7/PrinterErrors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Kyu7.PrinterErrors
+{
+    public class SolutionClass
+    {
+        public static string PrinterError(String s)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>
+            {
+              {'a',1}, {'b', 1}, {'c',1}, {'d', 1}, {'e',1}, {'f', 1}, {'g',1}, {'h', 1},
+              {'i',1}, {'j', 1}, {'k',1}, {'l', 1}, {'m',1}
+            };
+
+            int incorrectCount = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!map.ContainsKey(s[i]))
+                {
+                    incorrectCount++;
+                }
+            }
+            return $"{incorrectCount}/{s.Length}";
+        }
+    }
+}
---
+++ KataTest/Kyu7/PrinterErrorTest.cs
@@ -0,0 +1,18 @@
+﻿namespace KataTest.Kyu7
+{
+    using Kata.Kyu7.PrinterErrors;
+
+    [TestFixture]
+    public static class PrinterTests
+    {
+
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Testing PrinterError");
+            string s = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+            Assert.That(SolutionClass.PrinterError(s), Is.EqualTo("3/56"));
+        }
+    }
+
+}
