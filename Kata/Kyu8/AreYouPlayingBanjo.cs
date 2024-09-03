f1e9dc176b7b25ece12b7dfd074c4295bf3a275a
chitangchin <chitangb.chin@gmail.com>
2024-09-03 09:24:05 -0400

    Kyu 8: Are you playing banjo

---
+++ Kata/Kyu8/AreYouPlayingBanjo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Kyu8.AreYouPlayingBanjo
+{
+
+    public class SolutionClass
+    {
+        public static string AreYouPlayingBanjo(string name)
+        {
+            //Implement me
+            if (name[0] == 'r' || name[0] == 'R') return $"{name} plays banjo";
+            return $"{name} does not play banjo";
+        }
+    }
+}
---
+++ KataTest/Kyu8/AreYouPlayingBanjoTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTest.Kyu8
+{
+    using NUnit.Framework;
+    using Kata.Kyu8.AreYouPlayingBanjo;
+
+    [TestFixture]
+    public class AreYouPlayingBanjo
+    {
+        [Test]
+        public static void Martin()
+        {
+            Assert.That(SolutionClass.AreYouPlayingBanjo("Martin"), Is.EqualTo("Martin does not play banjo"));
+        }
+
+        [Test]
+        public static void Rikke()
+        {
+            Assert.That(SolutionClass.AreYouPlayingBanjo("Rikke"), Is.EqualTo("Rikke plays banjo"));
+        }
+
+        [Test]
+        public static void bravo()
+        {
+            Assert.That(SolutionClass.AreYouPlayingBanjo("bravo"), Is.EqualTo("bravo does not play banjo"));
+        }
+
+        [Test]
+        public static void rolf()
+        {
+            Assert.That(SolutionClass.AreYouPlayingBanjo("rolf"), Is.EqualTo("rolf plays banjo"));
+        }
+    }
+}
